3ff9789908c621725ee83c706be3c4b5b4dd3610
Jakub Mucha <jakub.mucha@icloud.com>
2020-10-06 01:19:30 +0200

    path changes
    Signed-off-by: Jakub Mucha <jakub.mucha@icloud.com>

--- generate-dockerfile.js
+++ generate-dockerfile.js
@@ -1,20 +1,20 @@
 const fs = require('fs');
 const dft = require('dockerfile-template');
-const dockerfileTemplate = fs.readFileSync('./Dockerfile.template', 'utf8');
+const dockerfileTemplate = fs.readFileSync('../Dockerfile.template', 'utf8');
 const args = require('minimist')(process.argv.slice(2))._;
 const tags = typeof args[0] === 'string' ? args[0].split(',') : args;
-const versionsFilePath = `${process.cwd()}/versions.json`;
+const versionsFilePath = '../versions.json';
 
 if (!fs.existsSync(versionsFilePath)) {
 	return console.error('versions.json file is missing!');
 }
 
 for (const tag of tags) {
-	const versions = require(`${process.cwd()}/versions.json`);
+	const versions = require(versionsFilePath);
 	versions.TAG = tag;
 
 	const updatedDockerfile = dft.process(dockerfileTemplate, versions);
-	const dockerfilePath = `${process.cwd()}/${tag}`;
+	const dockerfilePath = `../${tag}`;
 
 	if (!fs.existsSync(dockerfilePath)) {
 		fs.mkdirSync(dockerfilePath);
