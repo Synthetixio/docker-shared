5eab1df42e82c2480d23de0324315b86730f0040
Jakub Mucha <jakub.mucha@icloud.com>
2021-08-07 20:06:23 +0200

    feat: new version of dockerfile template generation script
    Signed-off-by: Jakub Mucha <jakub.mucha@icloud.com>

--- generate-dockerfile.js
+++ generate-dockerfile.js
@@ -1,27 +1,90 @@
-const fs = require('fs');
-const dft = require('dockerfile-template');
-const dockerfileTemplate = fs.readFileSync('../Dockerfile.template', 'utf8');
-const args = require('minimist')(process.argv.slice(2))._;
-const tags = typeof args[0] === 'string' ? args[0].split(',') : args;
-const versionsFilePath = '../versions.json';
+import { promises as fs } from 'fs';
+import fse from 'fs-extra';
+import nunjucks from 'nunjucks';
+import { default as rimrafCallback } from 'rimraf';
+import { promisify } from 'util';
+import { dirname } from 'path';
 
-if (!fs.existsSync(versionsFilePath)) {
-	return console.error('versions.json file is missing!');
-}
+const configFile = '../config.json';
+const templateFile = '../templates/base.Dockerfile.njk';
+const outputDirectory = '../output';
 
-for (const tag of tags) {
-	const versions = require(versionsFilePath);
-	versions.TAG = tag;
+const rimraf = promisify(rimrafCallback);
 
-	const updatedDockerfile = dft.process(dockerfileTemplate, versions);
-	const dockerfilePath = `../${tag}`;
+async function getVersions(file) {
+	const config = await fse.readJson(file);
+	const versions = [];
 
-	if (!fs.existsSync(dockerfilePath)) {
-		fs.mkdirSync(dockerfilePath);
-		fs.writeFileSync(`${dockerfilePath}/Dockerfile`, updatedDockerfile);
-	} else {
-		fs.writeFileSync(`${dockerfilePath}/Dockerfile`, updatedDockerfile);
+	for (const distroVersion of config.distro) {
+		for (const nodeVersion of config.nodeVersion) {
+			versions.push({
+				distro: distroVersion,
+				nodeVersion: nodeVersion,
+				alpineVersion: config.alpineVersion,
+				alpineSha256: config.alpineSha256,
+				ubuntuVersion: config.ubuntuVersion,
+				ubuntuSha256: config.ubuntuSha256,
+				debianVersion: config.debianVersion,
+				debianSha256: config.debianSha256,
+				yarnVersion: config.yarnVersion,
+				npmVersion: config.npmVersion,
+				vercelVersion: config.vercelVersion,
+			});
+		}
 	}
 
-	console.log(`Dockerfile for ${tag} generated with success.`);
+	return versions;
 }
+
+async function compileTemplate(file) {
+	const templateContents = await fs.readFile(file, 'utf-8');
+	return nunjucks.compile(templateContents);
+}
+
+async function render(template, context) {
+	return new Promise((resolve, reject) => {
+		nunjucks.render(template, context, function (error, result) {
+			if (error) {
+				reject(error);
+			}
+
+			resolve(result);
+		});
+	});
+}
+
+async function createDockerfile(template, file, versions) {
+	const dockerfile = await render(template, versions);
+	const directory = dirname(file);
+	await fs.mkdir(directory, { recursive: true });
+	await fs.writeFile(file, dockerfile, 'utf-8');
+
+	return {
+		...versions,
+		file,
+	};
+}
+
+async function createDockerfiles(template, versions) {
+	return Promise.all(
+		versions.map(version => {
+			const nodeVersionParts = version.nodeVersion.split('.');
+			const nodeFolderName = `${nodeVersionParts[0]}.${nodeVersionParts[1]}`;
+			createDockerfile(template, `${outputDirectory}/${version.distro}/${nodeFolderName}/Dockerfile`, version);
+		}),
+	);
+}
+
+async function bootstrap() {
+	await rimraf(outputDirectory);
+
+	const versions = await getVersions(configFile);
+	const template = await compileTemplate(templateFile);
+
+	await fs.mkdir(outputDirectory);
+
+	const files = await createDockerfiles(template, versions);
+	await fs.writeFile(`${outputDirectory}/data.json`, JSON.stringify(files, null, 2));
+}
+
+bootstrap().then(() => console.log('Dockerfiles generated'));
